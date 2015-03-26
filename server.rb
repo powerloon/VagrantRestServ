cada8dae56ec9a0f6b68afa23dabbc965e912947
Ben Singer <benjamin.singer@helpsystems.com>
2015-03-26 15:36:59 -0500

    added a bunch of stuff

--- server.rb
+++ server.rb
@@ -5,57 +5,65 @@ require 'json'
 
 puts "Starting server..."
 
-configure do    
 
-  class CommandRunner  
-    def initialize  
-      # Instance variables  
-      @os ||= (
-        host_os = RbConfig::CONFIG['host_os']
-        case host_os
-        when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-          :windows
-        when /darwin|mac os/
-          :macosx
-        when /linux/
-          :linux
-        when /solaris|bsd/
-          :unix
-        else
-          raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
-        end
-      )  
-    end  
-    
-    def run_command(command)
-      if @os == :windows
-        res = run_win(command)
+
+class CommandRunner  
+  def initialize  
+    # Instance variables  
+    @os ||= (
+      host_os = RbConfig::CONFIG['host_os']
+      case host_os
+      when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+        :windows
+      when /darwin|mac os/
+        :macosx
+      when /linux/
+        :linux
+      when /solaris|bsd/
+        :unix
       else
-        res = run_shell(command)
+        raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
       end
-      res
-    end 
-    
-    def run_shell(command)
-      #system(command)
-      system("ifconfig")
-    end
+    )  
+  end  
+  
+  def run_command(command)
+    # if @os == :windows
+    #   res = run_win(command)
+    # else
+    #   res = run_shell(command)
+    # end
+    # res
+    `#{command}`
+  end 
+  
+  # def run_shell(command)
+  #   #system(command)
+  #   system("ifconfig")
+  # end
 
-    def run_win(command)
-      `"#{command}"`
-    end 
-    
-  end
- 
+  # def run_win(command)
+  #   `#{command}`    
+  # end 
+  
+end    
+
+
+configure do
+  set :public_folder, '.'
 end
 
+cr = CommandRunner.new
+
 get '/' do
-  #send_file File.expand_path('index.html', settings.public_folder)
-  @cr = CommandRunner.new
-  res = @cr.run_command(nil)
-  puts typeof res
-  res = res.to_s
-  res
+  send_file File.expand_path('index.html', settings.public_folder)
+end
+
+post '/run' do  
+  request.body.rewind
+  rb = JSON.parse(request.body.read)
+  res = cr.run_command(rb["command"])
+  res.gsub(/(?:\n\r?|\r\n?)/, '<br>')
 end
 
 post '/login' do
